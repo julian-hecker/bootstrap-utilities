dd8871941e95b2ff3263dd5cabde940e8328eddb
julian-hecker <julianheckerdev@gmail.com>
2019-07-13 01:46:50 -0400

    update gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,17 @@ const cssnano = require('cssnano');
 const del = require('del');
 const gulp = require('gulp');
 const gulpPostcss = require('gulp-postcss');
+const gulpRename = require('gulp-rename');
 const gulpSass = require('gulp-sass');
 
 const paths = {
-  css: {
-    src: 'src/css/**/*.css',
-    dest: 'dist/css/'
-  },
+  // css: {
+  //   src: 'src/css/**/*.css',
+  //   dest: 'dist/css/'
+  // },
   scss: {
     src: 'src/scss/**/*.+(scss|sass)',
-    dest: 'src/css/'
+    dest: 'dist/css/'
   }
 };
 
@@ -23,21 +24,25 @@ function clean() {
   ])
 }
 
-function css() {
-  return gulp.src(paths.css.src)
-    .pipe(gulpPostcss( [autoprefixer( {remove: false} ), cssnano()] ))
-    .pipe(gulp.dest(paths.css.dest));
-}
+// function css() {
+//   return gulp.src(paths.css.src)
+//     .pipe(gulpPostcss( [autoprefixer( {remove: false} ), cssnano()] ))
+//     .pipe(gulp.dest(paths.css.dest));
+// }
 
 function scss() {
   return gulp.src(paths.scss.src)
     .pipe(gulpSass())
+    .pipe(gulpPostcss( [autoprefixer( {remove: false} )] ))
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(gulpPostcss( [cssnano] ))
+    .pipe(gulpRename("bootstrap-utilities.min.css"))
     .pipe(gulp.dest(paths.scss.dest));
 }
 
 exports.clean = clean;
-exports.css = css;
+// exports.css = css;
 exports.scss = scss;
 
-exports.build = gulp.series(clean, scss, css);
-exports.default = gulp.series(clean, scss, css);
+exports.build = gulp.series(clean, scss);
+exports.default = gulp.series(clean, scss);
